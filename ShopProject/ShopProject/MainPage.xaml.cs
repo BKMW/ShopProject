e82f6a6989218cb280169ae808943eaca1d01186
Fawzi Benamor <f.benamor@root.sa>
2018-03-07 09:58:47 +0100

    Add project files.

---
+++ ShopProject/ShopProject/MainPage.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShopProject
+{
+	public partial class MainPage : ContentPage
+	{
+	    private List<Product> products = new List<Product>
+	    {
+	      
+	            new Product
+	            {
+	                Image="a.jpg",
+	                Name="Gucci Hand Bag Light Brown",
+	                Status="Almost New",
+	                Price="3000.00 SAR",
+	                Categry = "Shoes",
+	                IsVisible = true
+
+	            },
+	            new Product
+	            {
+	                Image="a.jpg",
+	                Name="Boss Hand",
+	                Status="Almost New",
+	                Price="3000.00 SAR",
+	                Categry = "Shoes",
+	                IsVisible = true
+
+	            },
+	        
+	            new Product
+	            {
+	                Image ="a.jpg",
+	                Name="Clarks Shoes",
+	                Status="Almost New",
+	                Price="3000.00 SAR",
+	                Categry = "Shoes",
+	                IsVisible = true
+
+	            },
+	            new Product
+	            {
+	                Image="a.jpg",
+	                Name="Adidas Shoes",
+	                Status="Almost New",
+	                Price="3000.00 SAR",
+	                Categry = "Shoes",
+	                IsVisible = true
+
+	            },
+	       
+	            new Product
+	            {
+	                Image ="a.jpg",
+	                Name="Levis Clothes",
+	                Status="Almost New",
+	                Price="3000.00 SAR",
+	                Categry = "Shoes",
+	                IsVisible = true
+
+	            }
+	    };
+
+
+        private List<ProductPair> productsPairs;
+	    
+        public MainPage()
+		{
+			InitializeComponent();
+
+		    var category = new List<string>()
+		        { "Shoes",
+		          "Bags",
+		          "Accessories",
+                  "Travel"
+		        };
+            
+		    productsPairs = new List<ProductPair>
+		    {
+		        new ProductPair(
+		           /* new Product
+		            {
+		                Image="a.jpg",
+		                Name="Gucci Hand Bag Light Brown",
+		                Status="Almost New",
+		                Price="3000.00 SAR",
+		                Categry = "Shoes",
+		                IsVisible = true
+
+		            }*/products[0],
+		           /* new Product
+		            {
+		                Image="a.jpg",
+		                Name="Boss Hand",
+		                Status="Almost New",
+		                Price="3000.00 SAR",
+		                Categry = "Shoes",
+		                IsVisible = true
+
+		            }*/products[1]),
+		        new ProductPair(
+		            /*new Product
+		            {
+		                Image ="a.jpg",
+		                Name="Clarks Shoes",
+		                Status="Almost New",
+		                Price="3000.00 SAR",
+		                Categry = "Shoes",
+		                IsVisible = true
+
+		            },
+		            new Product
+		            {
+		                Image="a.jpg",
+		                Name="Adidas Shoes",
+		                Status="Almost New",
+		                Price="3000.00 SAR",
+		                Categry = "Shoes",
+		                IsVisible = true
+
+		            }*/products[2],products[3]),
+		        new ProductPair(
+		          /*  new Product
+		            {
+		                Image ="a.jpg",
+		                Name="Levis Clothes",
+		                Status="Almost New",
+		                Price="3000.00 SAR",
+		                Categry = "Shoes",
+		                IsVisible = true
+
+		            }*/products[4],null)
+		    };
+
+            ProductslistView.ItemsSource = productsPairs;
+
+        }
+	    public void RegroupByCategry(object sender, EventArgs e)
+	    {
+	        var keyword = MainSearchBar.Text;
+	        IEnumerable<ProductPair> searchReasult = productsPairs.Where(product => product.Item1.Name.ToLower().Contains(keyword.ToLower()));
+
+	        /*   IEnumerable<Product> searchReasult = products.Where(product => product.Name.ToLower().Contains(keyword.ToLower()));
+
+               productsPairs = new List<ProductPair>
+               {
+                   new ProductPair(
+                       products[0],products[1]
+
+                  )
+
+               };*/
+
+
+	        ProductslistView.ItemsSource = searchReasult;
+	    }
+        public void Search(object sender, EventArgs e)
+	    {
+	        var keyword = MainSearchBar.Text;
+	       IEnumerable<ProductPair> searchReasult = productsPairs.Where(product => product.Item1.Name.ToLower().Contains(keyword.ToLower()));
+
+	     /*   IEnumerable<Product> searchReasult = products.Where(product => product.Name.ToLower().Contains(keyword.ToLower()));
+
+	        productsPairs = new List<ProductPair>
+	        {
+	            new ProductPair(
+	                products[0],products[1]
+
+               )
+
+	        };*/
+
+
+        ProductslistView.ItemsSource = searchReasult;
+	    }
+    }
+}
---
+++ ShopProject/ShopProject/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopProject
+{
+    public class Product
+    {
+        public String Image { get; set; }
+        public String Name { get; set; }
+        public String Status { get; set; }
+        public String Price { get; set; }
+        public String Categry { get; set; }
+        public bool IsVisible { get; set; }
+
+        public Product()
+        {
+            this.Image = Image;
+            this.Name = Name;
+            this.Status = Status;
+            this.Price = Price;
+            this.Categry = Categry;
+            this.IsVisible = true;
+        }
+    }
+}
